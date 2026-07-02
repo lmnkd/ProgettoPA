8201b129e622aa359967fa74de4243de438304c9
lmnkd <amm.ing.2025@outlook.it>
2026-07-02 10:55:43 +0200

    levati commenti

--- src/service/AuthService.ts
+++ src/service/AuthService.ts
@@ -23,9 +23,7 @@ export class AuthService {
     // Funzione login che evita email sbagliata, controlla password e crea un JWT token all'avvenuto accesso
     async login(email: string, password: string): Promise<string> {
         const user = await userDao.findByEmail(email);
-         console.log("USER RAW:", JSON.stringify(user?.dataValues)); // tutti i campi raw dal DB
-         console.log("PASSWORD HASH:", user?.passwordHash); // deve essere non-null
-    
+        
         if (!user) {
             const err = new Error("Invalid credentials");
             err.name = AppErrorsName.INVALID_CREDENTIALS;
@@ -34,16 +32,13 @@ export class AuthService {
 
         
         const valid = await bcrypt.compare(password, user.passwordHash);
-        console.log("PASSWORD RICEVUTA:", JSON.stringify(password)); // JSON.stringify mostra eventuali caratteri nascosti
-        console.log("BCRYPT VALID:", valid);
+        
         if (!valid) {
             const err = new Error("Invalid credentials");
             err.name = AppErrorsName.INVALID_CREDENTIALS;
             throw err;
         }
-        console.log("PASSWORD RICEVUTA:", JSON.stringify(password)); // JSON.stringify mostra eventuali caratteri nascosti
-        console.log("BCRYPT VALID:", valid);
-
+        
         const payload: AppJwtPayload = {
             cf: user.cf,
             roles: toRolesArray(user.role),
